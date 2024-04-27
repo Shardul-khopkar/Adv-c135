f7e425d223e12caad6e5aaf4fc295dbd41e5fa38
Shardul-khopkar <80389421+Shardul-khopkar@users.noreply.github.com>
2024-04-27 16:17:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
-video = "";
-status = "";
+let status = ""
+let input = ""
+objects = []
 
-
+const synth = window.speechSynthesis
 
 function setup(){
-    canvas =  createCanvas(480, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(480, 380);
-    video.hide();
- }
- 
- function draw(){
-     image(video, 0, 0, 480, 380);
- }
-
+    canvas = createCanvas(380,380)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(480,380)
+    video.hide()
+}
+function draw(){
+    image(video,0,0,480,380)
+    
+    if(status!=""){
+        objDetector.detect(video,gotResults)
+        for(i = 0;i<objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            fill(255,0,0)
+            confidencePercent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " "+confidencePercent+"% ",objects[i].x+5,objects[i].y+15)
+            noFill()
+            stroke(255,0,0)
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+            if(input == objects[i].label){
+                document.getElementById("checkingIfFound").innerHTML = "Object Name : "+input
+                video.stop()
+                objDetector.detect(gotResults)
+                const utterThis = new SpeechSynthesisUtterance(input+" found");
+                synth.speak(utterThis)
+            }else{
+                document.getElementById("checkingIfFound").innerHTML = "Object not found"
+            }
+        }
+    }
+    
+}
 function start(){
-    objectDetetctor = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    input_text = document.getElementById("input").value;
-}
+    objDetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+    input  = document.getElementById("inp").value 
 
+}
 function modelLoaded(){
-    console.log("Model loaded");
-    status = "true";
+    status="true"
 }
-
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }else{
+        objects = results
+    }
+}
